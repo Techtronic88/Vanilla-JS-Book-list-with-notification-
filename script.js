234581e9830d54ad1b1842527e504930c503a5bf
DoanThomas88 <38048770+Techtronic88@users.noreply.github.com>
2019-04-16 19:12:26 +0700

    Added new functions
    Now the booklist can update itself each time we add in new Book. 
    Input fields clear out after each submission
    Still working out how to add in delete button

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ class Book {
     }
 }
 
-
 // UI CLASS:  We will handle UI tasks with this section 
 class UI {
     static displayBooks () {
@@ -22,9 +21,9 @@ class UI {
                 isbn: '45545'
             },
             {
-                title: 'Book Two',
-                author: 'Jane Doe',
-                isbn: '45545'
+                title: 'Book Three',
+                author: 'Peter Todd',
+                isbn: '66765'
             }
 
         ];
@@ -39,12 +38,52 @@ class UI {
               <td>${book.title}</td>
               <td>${book.author}</td>
               <td>${book.isbn}</td>
+              <td><a href ="#" class="btn btn-danger btn-smdelete">X</a></td>
               `;  
     list.appendChild(row);    
+        
+    }
+    // This class is utilized later on for us to clear out the input fields
+    static clearField() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+    }
 
+    static deleteBook(el) {
+        if(el.classList.contains('delete')){
+            console.log('Test');
+            el.parentElement.parentElement.remove();
+        }    
     }
 }
-// 
+// EVENT: ADD A BOOK
+
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+
+    // Fist we need to prevent actualy submit
+    e.preventDefault();
+
+    // Now we will grab the form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // Instatatiate book Class
+    const book = new Book(title, author, isbn);
+    
+    // Now books get added each time we click the submit button by accessing UI.addBookToList();
+    UI.addBookToList(book);
+
+    // Once submited we want all existing texts in the input field to dissepear
+    UI.clearField();
+
+    // EVENT: We would like to remove a book if we don't want it anymore
+    // 
+    document.querySelector('#book-list').addEventListener('click', (e) => {
+    UI.deleteBook(e.target)
+    })
+});
 
 // EVENT: Display list of BOOKS to the DOM... 
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
