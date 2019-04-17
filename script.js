6186545e63501d95163d2061f62f3a2f7e5bd266
Techtronic88 <Thomasdoan88@gmail.com>
2019-04-17 14:55:10 +0700

    Wrote logic for the pop up to appear according to trigger events.. Remove - pop up confirm, Added book pop up confirm

--- script.js
+++ script.js
@@ -18,13 +18,9 @@ class UI {
             {
                 title: 'Book Two',
                 author: 'Jane Doe',
-                isbn: '45545'
+                isbn: '4554576'
             },
-            {
-                title: 'Book Three',
-                author: 'Peter Todd',
-                isbn: '66765'
-            }
+        
 
         ];
         const books = storedBooks;
@@ -38,27 +34,45 @@ class UI {
               <td>${book.title}</td>
               <td>${book.author}</td>
               <td>${book.isbn}</td>
-              <td><a href ="#" class="btn btn-danger btn-smdelete">X</a></td>
+              <td><a href ="#" class="btn btn-danger btn-sm delete">X</a></td>
               `;  
     list.appendChild(row);    
         
     }
+    // NOTE: we target the parentElement of parentElement so the entire row can be removed
+    // Otherwise only remove button will be removed when we click on it.
+    static deleteBook(el) {
+        if(el.classList.contains('delete')) {
+          el.parentElement.parentElement.remove();
+        }
+      }
+
+     // WE want FANCY POP UP to appear when input fields are empty which makes the app much more aesthetic pleasant. 
+     // We will build the pop up from scratch and insert it into the UI..
+     static showAlert(message, className) {
+     const list = document.querySelector('.container');
+     const div = document.createElement('div');
+           div.className = `alert alert-${className}`;
+           div.appendChild(document.createTextNode(message));
+     const form = document.querySelector('#book-form');
+           list.insertBefore(div, form);
+     
+
+    // Make the POP UP disappear after 4 seconds
+    setTimeout(() => document.querySelector('.alert').remove(), 2000);
+
+
+}
+
+
     // This class is utilized later on for us to clear out the input fields
     static clearField() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
         document.querySelector('#isbn').value = '';
     }
-
-    static deleteBook(el) {
-        if(el.classList.contains('delete')){
-            console.log('Test');
-            el.parentElement.parentElement.remove();
-        }    
-    }
 }
 // EVENT: ADD A BOOK
-
 document.querySelector('#book-form').addEventListener('submit', (e) => {
 
     // Fist we need to prevent actualy submit
@@ -69,21 +83,33 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
+    // We need to validate the input field make sure they are not empty
+    if (title === '' || author === '' || isbn === '') {
+        UI.showAlert('Please fill in all fields', 'danger')
+    } else {
+
     // Instatatiate book Class
     const book = new Book(title, author, isbn);
     
     // Now books get added each time we click the submit button by accessing UI.addBookToList();
     UI.addBookToList(book);
 
+    // Show success message
+    UI.showAlert('Book Added', 'success')
+
     // Once submited we want all existing texts in the input field to dissepear
     UI.clearField();
-
-    // EVENT: We would like to remove a book if we don't want it anymore
-    // 
-    document.querySelector('#book-list').addEventListener('click', (e) => {
-    UI.deleteBook(e.target)
-    })
+  }
 });
+
+// EVENT: We would like to remove a book if we don't want it anymore
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    // Remove book from UI
+    UI.deleteBook(e.target);
+
+    UI.showAlert('Book Removed', 'success');
+
+})
 
 // EVENT: Display list of BOOKS to the DOM... 
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
